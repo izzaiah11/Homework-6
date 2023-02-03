4c46b15f08b0da33526435b235fac723374d5abf
Isaiah <114616212+izzaiah11@users.noreply.github.com>
2023-02-03 11:05:25 -0800

    started

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+document.querySelector(".API").addEventListener('click', function(){
+    var city = document.querySelector(".search").value 
+    var api = "https://api.openweathermap.org/data/2.5/weather?q="+city + "&appid=b27af2d7ef980ed996b17bdc5e1bce82"
+    fetch(api)
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(data){
+        console.log(data)
+    })
+})
